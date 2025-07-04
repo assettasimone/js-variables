67bfb1e7efe78fb9423fb22601af396266172d40
assettasimone <assettasimone@gmail.com>
2025-07-04 18:03:23 +0200

    add second

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -10,6 +10,11 @@ console.log(name)
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+const number = 3;
+const text = "questa è una stringa"
+const bool = false;
+console.log(number, text, bool)
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
